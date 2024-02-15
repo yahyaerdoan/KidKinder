d2f9e523793a683a8d5d571c70d8e870aa59c103
Yahya Erdogan <yahyacanerdogan@outlook.com>
2024-02-14 20:09:22 -0600

    Worked on contact for viewbag and dataAnnotations

---
+++ KidKinder/Controllers/CommunicationController.cs
@@ -0,0 +1,30 @@
+﻿using KidKinder.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KidKinder.Controllers
+{
+    public class CommunicationController : Controller
+    {
+        // GET: Communication
+        KidKinderContext kidKinderContex = new KidKinderContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult CommunicationPartial()
+        {
+            ViewBag.description = kidKinderContex.Communications.Select(d => d.Descripction).FirstOrDefault();
+            ViewBag.address = kidKinderContex.Communications.Select(a => a.Address).FirstOrDefault();
+            ViewBag.email = kidKinderContex.Communications.Select(e => e.Email).FirstOrDefault();
+            ViewBag.phone = kidKinderContex.Communications.Select(p => p.Phone).FirstOrDefault();
+            ViewBag.day = kidKinderContex.Communications.Select(d => d.Day).FirstOrDefault();
+            ViewBag.hour = kidKinderContex.Communications.Select(h => h.Hour).FirstOrDefault();
+            return PartialView();
+        }
+    }
+}
---
+++ KidKinder/Models/SendMessageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KidKinder.Models
+{
+    public class SendMessageViewModel
+    {
+        [Required(ErrorMessage = "Can't be empty")]
+        [StringLength(30, ErrorMessage = "Max 30 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Can't be empty")]
+        [StringLength(30, ErrorMessage = "Max 30 characters")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Can't be empty")]
+        [StringLength(30, ErrorMessage = "Max 30 characters")]
+        [EmailAddress(ErrorMessage ="Use valid mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Can't be empty")]
+        [StringLength(30,ErrorMessage = "Max 30 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Can't be empty")]
+        [StringLength(30, ErrorMessage = "Max 30 characters")]
+        public string Header { get; set; }
+
+        [Required(ErrorMessage = "Can't be empty")]
+        [StringLength(30, ErrorMessage = "Max 30 characters")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Can't be empty")]
+        [StringLength(30, ErrorMessage = "Max 30 characters")]
+        [MinLength(10,ErrorMessage ="Must more 10 characters")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Can't be empty")]
+        [StringLength(30, ErrorMessage = "Max 30 characters")]
+        public string Message { get; set; }
+    }
+}
