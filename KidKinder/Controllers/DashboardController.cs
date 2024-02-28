db85712cd910b8628677296fef621abc25270088
Yahya Erdogan <yahyacanerdogan@outlook.com>
2024-02-28 05:32:56 -0600

    Worked on dashboard

--- KidKinder/Controllers/DashboardController.cs
+++ KidKinder/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KidKinder.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,14 @@ namespace KidKinder.Controllers
     public class DashboardController : Controller
     {
         // GET: Dashboard
+        KidKinderContext kidKinderContext = new KidKinderContext();
         public ActionResult Index()
         {
+            ViewBag.BrachCount = kidKinderContext.Teachers.Where(x => x.BranchId == kidKinderContext.Branches.Where(z => z.Name == "Asp.Net").Select(y => y.BranchId).FirstOrDefault()).Count();
+
+            ViewBag.AveragaPrice = kidKinderContext.ClassRooms.Average(z => z.Price).ToString("0.00");
+
+            ViewBag.TeacherCount = kidKinderContext.Teachers.Count();
             return View();
         }
     }
